e3b874ea206fc6ce6632c4d15c90e86bee250da9
Administrator <3531962676@qq.com>
2023-01-10 21:34:57 +0800

    基础完成,之后移动到分区

--- js/测试.js
+++ js/测试.js
@@ -1,11 +1,48 @@
 document.getElementById("btn").onclick = function() {
+    let name
+
     console.log(document.getElementById("名字").value)
-    console.log(document.getElementById("内容").value)
-    ExportTxt(document.getElementById("名字").value, document.getElementById("内容").value)
+    console.log(document.getElementById("作者").value)
+
+    if (document.getElementById("名字").value == "") {
+        name = "null";
+        console.log(name)
+    } else {
+        name = document.getElementById("名字").value;
+    }
+    
+    if (document.getElementById("作者").value == "") {
+        author = "Node";
+    } else {
+        author = document.getElementById("作者").value;
+    }
+
+    if (document.getElementById("模组版本").value == "") {
+        version = 114514;
+    } else {
+        version = document.getElementById("模组版本").value;
+    }
+
+    if (document.getElementById("游戏版本").value == "") {
+        minGameVersion = 136;
+    } else {
+        minGameVersion = document.getElementById("游戏版本").value;
+    }
+
+    mod = '{\n' +
+     '"name": "' + name +
+      '", \n"displayName":"' + name +
+      '", \n"author":"' + author +
+      '", \n"version":' + version +
+      ', \n"description":"' + document.getElementById("简介").value +
+      '", \n"minGameVersion":' + minGameVersion +
+       '\n}';
+    ExportTxt("mod", mod)
 }
 
 
 function ExportTxt(name, data) {
+    console.log(name)
     const urlObject = window.URL || window.webkitURL || window;
     const export_blob = new Blob([data]);
 
@@ -17,3 +54,5 @@ function ExportTxt(name, data) {
     ev.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
     save_link.dispatchEvent(ev);
 }
+
+
